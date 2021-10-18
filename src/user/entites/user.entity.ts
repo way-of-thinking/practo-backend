b565b977740bec3bcae385bcca7cb971d97f05f2
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-18 11:37:22 +0300

    create a user entity

--- src/user/entites/user.entity.ts
+++ src/user/entites/user.entity.ts
@@ -1,11 +1,37 @@
-import { CoreEntity } from "src/common/entities/core.entity";
-import { Column, Entity } from "typeorm";
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity } from 'typeorm';
 
+export enum UserRole {
+  Admin = 'Admin',
+  User = 'User',
+  Doctor = 'Doctor',
+  Supervisor = 'Supervisor',
+  MaketingManager = 'MaketingManager',
+  ProductManager = 'ProductManager',
+  ProjectManager = 'ProjectManager',
+  FinanceManager = 'FinanceManager',
+}
 
-@Entity({name:"users"})
-export class User extends CoreEntity{
-    @Column({ unique: true })
-    @Field((type) => String)
-    @IsEmail()
-    email: string;
+@Entity({ name: 'users' })
+export class User extends CoreEntity {
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
+
+  @Column({ type: 'enum', enum: UserRole })
+  role: UserRole;
+
+  @Column({ default: false })
+  verified: boolean;
 }
