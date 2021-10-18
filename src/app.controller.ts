489e575313fcd00ccee685d63b85b7f0e483535b
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-18 08:59:51 +0300

    add ji and @nestjs/config and configure database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/_ah/health')
+  getHealth(): string {
+    return this.appService.getHello();
+  }
 }
