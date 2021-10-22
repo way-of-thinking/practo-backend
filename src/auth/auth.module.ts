08006dc7ddaec3b1f5690911acd100731d7b7e53
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-22 11:30:28 +0300

    add jwt and jwt-strategy and install passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entites/user.entity';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [JwtStrategy],
+  exports: [PassportModule],
 })
 export class AuthModule {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,14 +5,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from 'src/user/entites/user.entity';
+import { Repository } from 'typeorm';
 import { JwtPayload } from './jwt-payload.interface';
-
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
-    @InjectRepository(UserRepository)
-    private userRepository: UserRepository,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
     private configService: ConfigService,
   ) {
     super({
@@ -22,9 +22,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload): Promise<User> {
-    const { username } = payload;
+    const { id } = payload;
 
-    const user = await this.userRepository.findOne({ username });
+    const user = await this.userRepository.findOne({ id });
 
     if (!user) {
       throw new UnauthorizedException();
