d3731ed95725e9f4bc88b0f02c04b2615b1594e8
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-23 06:00:17 +0300

    add backend validation pieps

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BackendBackendValidationPipe } from './common/shared/pipes/backendValidation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new BackendBackendValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3002);
 }
