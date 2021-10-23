7f9ec195b72674f61ca367b0cca5b0d0ffa01ff2
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-23 06:21:09 +0300

    add origin configure

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BackendBackendValidationPipe } from './common/shared/pipes/backendValidation.pipe';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'dev') {
+    app.enableCors({ origin: process.env.FRONTEND_URI });
+  } else {
+    app.enableCors({ origin: process.env.FRONTEND_URI });
+    logger.log(`Accepting requests from origin "${process.env.FRONTEND_URI}"`);
+  }
+
   app.useGlobalPipes(new BackendBackendValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3002);
+  logger.log(`Application listening on port ${process.env.PORT}`);
 }
 bootstrap();
