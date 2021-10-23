304ec3ed8834c42ccaebbe2db00b1bba873ab479
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2021-10-23 13:51:17 +0300

    user entity

--- src/user/entites/user.entity.ts
+++ src/user/entites/user.entity.ts
@@ -2,6 +2,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { BeforeInsert, BeforeUpdate, Column, Entity, Index } from 'typeorm';
+
 export enum UserRole {
   Admin = 'Admin',
   User = 'User',
@@ -19,7 +20,7 @@ export class User extends CoreEntity {
   @Column()
   email: string;
 
-  @Column({  nullable: true })
+  @Column({ nullable: true })
   @Index('UQ_user_username', { unique: true })
   username: string;
 
